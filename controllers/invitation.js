c5424fd0c8c36c298d57b29c5f286570af69d373
Chitwan Bindal <chitwan001@gmail.com>
2022-01-22 01:34:30 +0530

    first commit

--- controllers/invitation.js
+++ controllers/invitation.js
@@ -1,5 +1,19 @@
+const invitationModel = require('../models/invitations');
+const classModel = require('../models/classes');
 exports.createInvi = (req,res,next) => {
-    res.send({
-        response : req.body
+
+    classModel.findById(req.body.cid).then(data => {
+        const newInvi = new invitationModel({
+            bystudent : req.userId,
+            totutor : data.teachers,
+            forbatch : req.body.cid,
+            isaccepted : false
+        });
+        newInvi.save().then(data =>{
+            res.send({
+                response : 'ok'
+            })
+        })
+    
     })
 }
--- models/invitations.js
+++ models/invitations.js
@@ -2,17 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const invitationsSchema = new Schema({
-  title: {
-    type: String,
-    required : true
-  },
   bystudent : {
     type: Schema.Types.ObjectId,
     ref: 'Student',
     required : true
   },
   totutor : {
-    type: [Schema.Types.ObjectId],
+    type: [Schema.Types.ObjectId , String],
     ref: 'Tutor',
     required : true
   },
